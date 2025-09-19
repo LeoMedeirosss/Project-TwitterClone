68636d58e3ec3ff72fc602c0728e4bcb6bf6a312
LeoMedeirosss <lmf@cesar.school>
2025-09-19 19:25:19 -0300

    feat: primeira tela no feed

--- twitter-clone/app/index.tsx
+++ twitter-clone/app/index.tsx
@@ -2,8 +2,18 @@ import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello Twitter Clone 🚀</Text>
+    <View className="Conteiner" style={{ flex: 1, alignItems: "center", backgroundColor: "#000" }}>
+      <View className="Header" style={{width: '100%', height: 90, justifyContent: 'center', alignItems: 'center', backgroundColor: "#202020" , borderBottomWidth: 1, borderBottomColor: '#444', opacity: 0.7}}>
+        <View style={{width: '70%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 40}}>
+          <View style={{padding: 15,}}>
+            <Text style={{color: "#fff", fontWeight: "bold" }}>Para você</Text>
+            <View style={{borderBottomWidth: 3, borderBottomColor: '#1d9bf0', width: "105%", alignSelf: "center", borderRadius: 12 }} />
+          </View>
+          <View style={{padding: 15,}}>
+            <Text style={{color: "#fff"}}>Seguindo</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
